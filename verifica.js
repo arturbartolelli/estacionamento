fac5df5d98eaa0f059f47b89999b4ae00cca742d
IanAlas <alasian510@gmail.com>
2024-05-17 11:42:10 -0300

    Plate: check: verification sucessly and organization

--- verifica.js
+++ verifica.js
@@ -1,45 +1,49 @@
-//---- Fazendo esta parte primeiro -----
-//JDP 0A01 a JKR 9J99 Distrito Federal (DF)
-//OVM 0A01 a OVV 9J99 Distrito Federal (DF) 2ª sequência
-//OZW 0A01 a PBZ 9J99 Distrito Federal (DF) 3ª sequência
 
-let plate = "JKJ0A01";
-let regexDF1 = /^[J][D-K][P-R][0-9][A-J][0-9]{2}$/;
-let regexDF2 = /^[O][V][M-V][0-9][A-J][0-9]{2}$/;
-let regexDF3 = /^[O-P][B-Z][W-Z][0-9][A-J][0-9]{2}$/;
+function verificarPlacaEstado(plate) {
 
-if (regexDF1.test(plate)) {
-    console.log("A placa é do DISTRITO FEDERAL1.");
-}else if(regexDF2.test(plate)){
-    console.log("A placa é do DISTRITO FEDERAL2.");
-}else if(regexDF3.test(plate)){
-    console.log("A placa é do DISTRITO FEDERAL3.");
+    const dfSequencesDF = [
+        {start: "JDP0A01", end: "JKR9J99"},
+        {start: "OVM0A01", end: "OVV9J99"}, 
+        {start: "OZW0A01", end: "PBZ9J99"}   
+    ];
+    const dfSequencesMG = [
+        {start: "GKJ0A01", end: "HOK9J99"},
+        {start: "NXX0A01", end: "NYG9J99"}, 
+        {start: "OLO0A01", end: "OMH9J99"}, 
+        {start: "OOV0A01", end: "ORC9J99"},
+        {start: "OWH0A01", end: "OXK9J99"}, 
+        {start: "PUA0A01", end: "PZZ9J99"}, 
+        {start: "QMQ0A01", end: "QQZ9J99"}  
+    ];
+    const dfSequencesGO = [
+        {start: "KAV0A01", end: "KFC9J99"},
+        {start: "NFC0A01", end: "NGZ9J99"}, 
+        {start: "NJX0A01", end: "NLU9J99"}, 
+        {start: "NVO0A01", end: "NWR9J99"},
+        {start: "OGH0A01", end: "OHA9J99"}, 
+        {start: "OMI0A01", end: "OOF9J99"}, 
+        {start: "PQA0A01", end: "PRZ9J99"},
+        {start: "QRC0A01", end: "QTX9J99"}   
+    ];
+    plate = plate.replace(/ /g, "").toUpperCase();
+
+    for (let i = 0; i < dfSequencesDF.length; i++) {
+        if (dfSequencesDF[i].start <= plate && plate <= dfSequencesDF[i].end) {
+            return `"${plate}" esta placa é do DISTRITO FEDERAL`
+        }
+    }
+    for (let i = 0; i < dfSequencesMG.length; i++) {
+        if (dfSequencesMG[i].start <= plate && plate <= dfSequencesMG[i].end) {
+            return `"${plate}" esta placa é de MINAS GERAIS`
+        }
+    }
+    for (let i = 0; i < dfSequencesGO.length; i++) {
+        if (dfSequencesGO[i].start <= plate && plate <= dfSequencesGO[i].end) {
+            return `"${plate}" esta placa é de GOIÁS`
+        }
+    }
+    return false;
 }
-else {
-    console.log("A placa NÃO é do DISTRITO FEDERAL.");
-}
-
-//---- Fazendo esta parte segundo -----
-//KAV 0A01 a KFC 9J99 Goiás (GO)
-//NFC 0A01 a NGZ 9J99 Goiás (GO) 2ª sequência
-//NJX 0A01 a NLU 9J99 Goiás (GO) 3ª sequência
-//NVO 0A01 a NWR 9J99 Goiás (GO) 4ª sequência
-//OGH 0A01 a OHA 9J99 Goiás (GO) 5ª sequência
-//OMI 0A01 a OOF 9J99 Goiás (GO) 6ª sequência
-//PQA 0A01 a PRZ 9J99 Goiás (GO) 7ª sequência
-//QRC 0A01 a QTX 9J99 Goiás (GO) 8ª sequência
-
-let regexGO1 = /^[K][A-F][C-V][0-9][A-J][0-9]{2}$/;
-let regexGO2= /^[N][FG][C-Z][0-9][A-J][0-9]{2}$/;
-let regexGO3 = /^[N][J-L][U-X][0-9][A-J][0-9]{2}$/;
-
-if (regexGO1.test(plate)) {
-    console.log("A placa é do GOIÁS1.");
-}else if(regexGO2.test(plate)){
-    console.log("A placa é do GOIÁS2.");
-}else if(regexGO3.test(plate)){
-    console.log("A placa é do GOIÁS3.");
-}
-else {
-    console.log("A placa NÃO é do GOIÁS.");
-}
+console.log(verificarPlacaEstado("JDP 0A01"));  // Deve retornar true
+console.log(verificarPlacaEstado("PCX 9A78"));  // Deve retornar false
+console.log(verificarPlacaEstado("KBD 3C98"));
