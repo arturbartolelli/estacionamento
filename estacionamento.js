b62dda5a0102d9cc01e3f883023384f80691b1a8
arturbartonelli <arturbartonelli@gmail.com>
2024-05-24 14:46:25 -0300

    ajpsiodjasd

--- estacionamento.js
+++ estacionamento.js
@@ -1,21 +1,79 @@
-const enviar = document.querySelector("#botaoEntrada");
+const enviar = document.getElementById("botaoEntrada");
 
 var estacionamento = [];
 var horaEntrada, horaSaida;
+let matriz = new Array(10).fill().map(() => new Array(10).fill(null));
+
+let vagas = 100
 
 const modal = document.getElementById("dialog-box")
-const btnModal = document.getElementById("btnModal")
-const btnCloseModal = document.getElementById("btnClose")
+const closeModal = document.getElementById("btnClose")
+const contador = document.getElementById("contador")
+
+contador.innerHTML = `Vagas: ${vagas}`
+
+let grid = document.getElementById('grid');
+
+const criarDiv = (carro) => {
+
+    const card = document.createElement('div');
+    card.classList.add('card1');
+
+    const content = document.createElement('div');
+    content.classList.add('content');
+
+    const back = document.createElement('div');
+    back.classList.add('back');
+
+    const backContent = document.createElement('div');
+    backContent.classList.add('back-content');
+    backContent.innerHTML = `Placa: ${carro.placa}`;
 
 
-btnModal.onclick = () => {
-    modal.style.display = "block";
+    back.appendChild(backContent);
+    content.appendChild(back);
+    card.appendChild(content);
+    return card;
 }
 
-btnCloseModal.onclick = () => {
-    modal.style.display = "none";
+const adicionarCarro = () => {
+    contador.innerHTML = `Vagas: ${--vagas}`
+    grid.innerHTML = "";
+
+    matriz.forEach((linha, i) => {
+        linha.forEach((carro, j) => {
+            if (carro !== null) {
+                const div = criarDiv(carro);
+                div.setAttribute("data-posicao", `${i}-${j}`);
+
+                const btnRemove = document.createElement("button");
+                btnRemove.setAttribute("class", "btnRemove");
+                btnRemove.innerHTML = "X";
+                btnRemove.addEventListener("click", (evt) => {
+                    const linha = parseInt(posicao[0]);
+                    const coluna = parseInt(posicao[1]);
+                    modal.style.display = "block"
+                    removeCarro(linha, coluna);
+                });
+
+                div.querySelector('.back-content').appendChild(btnRemove);
+                grid.appendChild(div);
+
+            }
+        });
+    });
+    
+};
+
+
+const removeCarro = (linha, coluna) => {
+    matriz[linha][coluna] = null;
 }
 
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none"
+    console.log(matriz)
+})
 
 const pegarHoraEntrada = () => {
     let tempo = new Date;
@@ -25,12 +83,6 @@ const pegarHoraEntrada = () => {
     horaEntrada = horaIn*60 + minIn;
 }
 
-const pegarValor = () => {
-    const placa = document.querySelector('#placa').value;
-    pegarHoraEntrada();
-    estacionamento.push(placa)
-};
-
 const pegarHoraSaida = () => {
     let tempo = new Date;
     let horaIn = tempo.getHours();
@@ -39,7 +91,42 @@ const pegarHoraSaida = () => {
     horaSaida = horaIn*60 + minIn;
 }
 
-enviar.addEventListener("click", pegarValor);
+const criarCarro = (event) => {
+    event.preventDefault()
+    const placa = document.querySelector('#placa').value;
+    // pegarHoraEntrada();
+    const carro = new Carro(placa)
+    const posicaoCarro = inserirCarroAleatoriamente(carro, matriz)
+    carro.posicao = posicaoCarro
+    adicionarCarro()
+};
+
+const inserirCarroAleatoriamente = (carro, matriz) => {
+    let min = 0;
+    let max = 9;
+    let linha, coluna;
+
+    do {
+        linha = Math.floor(Math.random() * (max - min + 1)) + min;
+        coluna = Math.floor(Math.random() * (max - min + 1)) + min;
+    } while (matriz[linha][coluna] !== null);
+
+    matriz[linha][coluna] = carro;
+    return { linha, coluna };
+}
+
+const encontrarCarro = (carro, matriz) => {
+    for (let i = 0; i < matriz.length; i++) {
+        for (let j = 0; j < matriz[i].length; j++) {
+            if (matriz[i][j] === carro) {
+                return { linha: i, coluna: j };
+            }
+        }
+    }
+    return null;
+}
+
+enviar.addEventListener("click", criarCarro);
 
 function calculaPreco(hIn, hOut) {
     const tempoTotal = hOut - hIn
@@ -61,7 +148,14 @@ function calculaPreco(hIn, hOut) {
     }    
 }
 
-console.log(calculaPreco(0, 15))
+
+class Carro { 
+    constructor(placa) {
+        this.placa = placa;
+        this.posicao = {};
+    }
+}
+
 
 
 
